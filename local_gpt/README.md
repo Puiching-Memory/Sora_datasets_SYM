4ad5abbbfc1caa0627a040732e65970bc1171747
梦归云帆 <1138663075@qq.com>
2024-04-26 00:38:55 +0800

    Update GPT

--- local_gpt/README.md
+++ local_gpt/README.md
@@ -1,6 +1,5 @@
 # GPT服务器
 
-
 # 环境
 
 ### 数据库
@@ -8,6 +7,16 @@
 redis-5.0.14
 
 https://github.com/tporadowski/redis/releases
+
+### 本地GPT框架
+
+llama.cpp-python
+
+注意需要使用CUDA后端
+
+```
+pip install llama-cpp-python --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/<cuda-version>
+```
 
 # 网络
 
--- local_gpt/test_llmacpp.py
+++ local_gpt/test_llmacpp.py
@@ -1,5 +1,7 @@
+#https://github.com/abetlen/llama-cpp-python
+import time
 from llama_cpp import Llama
-model = Llama("/data/hf/Llama3-8B-Chinese-Chat.q4_k_m.GGUF", verbose=False, n_gpu_layers=-1)
+model = Llama(r"D:\GitHub\Llama3-8B-Chinese-Chat.q4_k_m.GGUF", verbose=False, n_gpu_layers=-1,n_threads=16,n_threads_batch=16)
 messages = [
     {"role": "system", "content": "你是一个疯狂的科学家大卫，你总是为了毁灭宇宙而努力。"},
     {"role": "user", "content": "你是谁？"},
